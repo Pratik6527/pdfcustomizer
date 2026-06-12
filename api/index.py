d8518814b27a82f0d365c3c361394579503b549d
Pratik6527 <Pratik6527@users.noreply.github.com>
2026-06-12 20:29:35 +0530

    Delete all files from master branch

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-# Add the project root to the sys.path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app import app
-
-# This allows Vercel serverless functions to pick up the `app` instance
